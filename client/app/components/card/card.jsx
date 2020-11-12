f6d1a4be8e55066bd9bd7a56dd9d2af3128d25b2
Katia-2020 <kateflights@gmail.com>
2020-11-12 23:04:45 +0000

    updated Card component

--- client/app/components/card/card.jsx
+++ client/app/components/card/card.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import backCard from './svg';
 import styles from './card.scss';
 
 const Card = (props) => {
   const {
-    image,
-    name,
-    theme,
     id,
+    name,
     onClick,
+    children,
   } = props;
 
   const handleClick = () => {
     onClick(name, id);
   };
 
-  const glyph = backCard;
-
   return (
     <div className={styles['card']} onClick={handleClick}>
-      {image && id ?
-        (<img src={image} alt="" />) :
-        (<i className={styles['card__icon']} dangerouslySetInnerHTML={{ __html: glyph }} />)}
+      {children}
     </div>
   );
 };

7c80984f36f59bc4d2fdda69a45ce578292dfb61
unknown <dlorenzo.05@gmail.com>
2020-11-12 12:19:08 +0100

    lint fixed

--- client/app/components/memory-game/memory-game.mock.js
+++ client/app/components/memory-game/memory-game.mock.js
@@ -1,43 +1,43 @@
 const images = [
   {
     name: 'ice cream',
-    url: 'https://images.unsplash.com/photo-1505394033641-40c6ad1178d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1406&q=80'
+    url: 'https://images.unsplash.com/photo-1505394033641-40c6ad1178d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1406&q=80',
   },
   {
     name: 'meat',
-    url: 'https://images.unsplash.com/photo-1575229228428-8779b8a883a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+    url: 'https://images.unsplash.com/photo-1575229228428-8779b8a883a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
   },
   {
     name: 'flower',
-    url: 'https://images.unsplash.com/photo-1502977249166-824b3a8a4d6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+    url: 'https://images.unsplash.com/photo-1502977249166-824b3a8a4d6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
   },
   {
     name: 'zebra',
-    url: 'https://images.unsplash.com/photo-1501706362039-c06b2d715385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=535&q=80'
+    url: 'https://images.unsplash.com/photo-1501706362039-c06b2d715385?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=535&q=80',
   },
   {
     name: 'birds',
-    url: 'https://images.unsplash.com/photo-1593473568361-97d48cbdc5c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+    url: 'https://images.unsplash.com/photo-1593473568361-97d48cbdc5c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
   },
   {
     name: 'thailand',
-    url: 'https://images.unsplash.com/photo-1492693377442-7f88103dc502?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+    url: 'https://images.unsplash.com/photo-1492693377442-7f88103dc502?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
   },
   {
     name: 'temple',
-    url: 'https://images.unsplash.com/photo-1569155829632-148eea0844af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=479&q=80'
+    url: 'https://images.unsplash.com/photo-1569155829632-148eea0844af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=479&q=80',
   },
   {
     name: 'cloud',
-    url: 'https://images.unsplash.com/photo-1573630309736-1f2bd189b509?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+    url: 'https://images.unsplash.com/photo-1573630309736-1f2bd189b509?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
   },
   {
     name: 'plane',
-    url: 'https://images.unsplash.com/photo-1496054682147-258414043211?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+    url: 'https://images.unsplash.com/photo-1496054682147-258414043211?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
   },
   {
     name: 'volcano',
-    url: 'https://images.unsplash.com/photo-1574786199452-c7a5f69fb6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
+    url: 'https://images.unsplash.com/photo-1574786199452-c7a5f69fb6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
   },
 ];
 

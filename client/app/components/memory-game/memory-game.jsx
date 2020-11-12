efc03c01044d29c970ca29f50f499e9372e99f3a
Katia-2020 <kateflights@gmail.com>
2020-11-12 23:03:33 +0000

    abstracted Image and Icon components and added them separately

--- client/app/components/memory-game/memory-game.jsx
+++ client/app/components/memory-game/memory-game.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Row, Column } from '../grid';
 import Card from '../card';
+import Image from '../image';
+import Icon from '../icon';
 import images from './memory-game.mock';
 import styles from './memory-game.scss';
 
@@ -9,35 +11,65 @@ class MemoryGame extends React.Component {
     super(props);
 
     this.state = {
-      currentCard: '',
-      currentId: 0,
-      previousId: 0,
-      previousCard: '',
+      // currentCard: '',
+      // currentId: 0,
+      // previousId: 0,
+      // previousCard: '',
+      currentCard: {
+        id: '',
+        name: '',
+      },
+      previousCard: {
+        id: '',
+        name: '',
+      },
       isClicked: false,
+      foundImages: [],
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(name, id) {
-    const { currentCard, currentId } = this.state;
+    const { currentCard, previousCard, foundImages, isClicked } = this.state;
     let newState;
 
-    if (currentCard === '') {
+    if (!currentCard.name) {
       newState = {
-        currentCard: name,
-        currentId: id,
-        isClicked: true,
+        currentCard: {
+          name,
+          id,
+        },
+        isClicked: !isClicked,
       };
     }
 
-    if (currentCard !== '') {
+    if (currentCard.name && currentCard.name !== previousCard.name) {
       newState = {
-        currentCard: name,
-        currentId: id,
-        previousId: currentId,
-        previousCard: currentCard,
-        isClicked: true,
+        currentCard: {
+          id,
+          name,
+        },
+        previousCard: {
+          id: currentCard.id,
+          name: currentCard.name,
+        },
+        isClicked: !isClicked,
+      };
+    }
+
+    if (currentCard.name && currentCard.name === previousCard.name) {
+      newState = {
+        currentCard: {
+          id: '',
+          name: '',
+        },
+        previousCard: {
+          id: '',
+          name: '',
+        },
+        isClicked: !isClicked,
+        foundImages: [...foundImages, currentCard.name],
       };
     }
 
@@ -45,15 +77,15 @@ class MemoryGame extends React.Component {
   }
 
   isSameCard() {
-    const { currentCard, previousCard, isClicked } = this.state;
+    const { currentCard, previousCard } = this.state;
 
-    return !!((currentCard === previousCard) && isClicked);
+    return !!(currentCard.name === previousCard.name);
   }
 
   render() {
     console.log(this.state);
 
-    const { currentId, previousId } = this.state;
+    const { currentCard, previousCard, isClicked } = this.state;
     const sameCard = this.isSameCard();
 
     return (
@@ -64,21 +96,31 @@ class MemoryGame extends React.Component {
               <Column>
                 <div className={styles['memory-game__item']}>
                   <Card
-                    image={(currentId === item.id) && item.url}
-                    name={item.name}
                     id={item.id}
+                    name={item.name}
                     onClick={this.handleClick}
-                  />
+                  >
+                    {(currentCard.id === item.id) ? (
+                      <Image url={item.url} />
+                      ) : (
+                        <Icon />
+                      )}
+                  </Card>
                 </div>
               </Column>
               <Column>
                 <div className={styles['memory-game__item']}>
                   <Card
-                    image={((currentId === item.id || previousId === item.id) && sameCard) && item.url}
-                    name={item.name}
                     id={item.id + images.length}
+                    name={item.name}
                     onClick={this.handleClick}
-                  />
+                  >
+                    {(currentCard.id === (item.id + images.length)) ? (
+                      <Image url={item.url} />
+                      ) : (
+                        <Icon />
+                      )}
+                  </Card>
                 </div>
               </Column>
             </>

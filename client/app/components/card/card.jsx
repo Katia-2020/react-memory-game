450cc48f492eb6baa7084c4eb08478620af0b0e0
unknown <dlorenzo.05@gmail.com>
2020-11-12 12:54:04 +0100

    onClick event passed as prop, handleClick function declared to call the event onClick, svg icon imported and declared as glyph variable, logic added in return to get the icon or the image depending on the prop that we get

--- client/app/components/card/card.jsx
+++ client/app/components/card/card.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import backCard from './svg';
 import styles from './card.scss';
 
 const Card = (props) => {
-  const { image, theme } = props;
+  const { image, theme, onClick } = props;
+
+  const handleClick = () => {
+    onClick();
+  };
+
+  const glyph = backCard;
 
   return (
-    <div className={styles.card} />
+    <div className={styles['card']} onClick={handleClick}>
+      {theme ?
+        (<i className={styles['card__icon']} dangerouslySetInnerHTML={{ __html: glyph }} />) :
+        (<img src={image} alt="" />)}
+    </div>
   );
 };
 

99ad8d10307a5850fbcf12e02aaa37abe24d7cbc
Katia-2020 <kateflights@gmail.com>
2020-11-12 11:22:18 +0000

    created Memory-Game component

--- client/app/components/memory-game/memory-game.jsx
+++ client/app/components/memory-game/memory-game.jsx
@@ -1,4 +1,28 @@
 import React from 'react';
+import { Row, Column } from '../grid';
+import Cart from '../cart';
 import styles from './memory-game.scss';
 
+class MemoryGame extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className={styles['memory-game']}>
+        <Row>
+          <Column>
+            <Cart />
+          </Column>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default MemoryGame;

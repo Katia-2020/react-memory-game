ac0264285a479b617665261ad95715d34c298885
Katia-2020 <kateflights@gmail.com>
2020-11-13 22:06:01 +0000

    added items to the array

--- client/app/components/memory-game/memory-game.mock.js
+++ client/app/components/memory-game/memory-game.mock.js
@@ -49,6 +49,56 @@ const images = [
     name: 'tomato',
     url: 'https://images.unsplash.com/photo-1566383444833-43afb88e5dc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=462&q=80',
   },
+  {
+    id: 11,
+    name: 'watermelon',
+    url: 'https://images.unsplash.com/photo-1595475207225-428b62bda831?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    id: 12,
+    name: 'pineapple',
+    url: 'https://images.unsplash.com/photo-1548805973-e8b0751bd0f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    id: 13,
+    name: 'banana',
+    url: 'https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    id: 14,
+    name: 'cucumber',
+    url: 'https://images.unsplash.com/photo-1566486189376-d5f21e25aae4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=489&q=80',
+  },
+  {
+    id: 15,
+    name: 'pumpkin',
+    url: 'https://images.unsplash.com/photo-1569976710208-b52636b52c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=476&q=80',
+  },
+  {
+    id: 16,
+    name: 'lemon',
+    url: 'https://images.unsplash.com/photo-1582087463261-ddea03f80e5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    id: 17,
+    name: 'orange',
+    url: 'https://www.visitsicily.info/wp-content/uploads/2018/11/arance-rosse-cavasenna.jpg',
+  },
+  {
+    id: 18,
+    name: 'kiwi',
+    url: 'https://images.unsplash.com/photo-1542400935-70190c63c242?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80',
+  },
+  {
+    id: 19,
+    name: 'strawberry',
+    url: 'https://www.gardenersdream.co.uk/images/strawberry-cambridge-favourite-bare-root-plants-p3542-25404_image.jpg',
+  },
+  {
+    id: 20,
+    name: 'tomato',
+    url: 'https://images.unsplash.com/photo-1566383444833-43afb88e5dc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=462&q=80',
+  },
 ];
 
 export default images;

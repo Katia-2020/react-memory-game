1c862ddc285fae461bccd024a38da079418c07b6
Katia-2020 <kateflights@gmail.com>
2020-11-14 18:44:02 +0000

    created Result component

---
+++ client/app/components/result/result.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Text from '../text';
+import styles from './result.scss';
+
+const Result = (props) => {
+  const { result } = props;
+
+  return (
+    <div className={styles['result']}>
+      <div className={styles['result__header']}>
+        <Text text="your score:" cases="uppercase" color="dark-blue" weight="bold" />
+      </div>
+      <div className={styles['result__body']}>
+        <Text text={result} size="medium" color="pink" weight="bold" />
+      </div>
+    </div>
+  );
+};
+
+export default Result;

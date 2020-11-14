71d553bb4a17319e54855c68cfd6b525409c7cfc
Katia-2020 <kateflights@gmail.com>
2020-11-14 18:43:10 +0000

    created Button component

---
+++ client/app/components/button/button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Text from '../text';
+import styles from './button.scss';
+
+const Button = (props) => {
+  const { text, onClick } = props;
+
+  const handleClick = () => {
+    onClick();
+  };
+
+  return (
+    <div className={styles.button} onClick={handleClick}>
+      <Text text={text} size="large" cases="uppercase" color="white" center />
+    </div>
+  );
+};
+
+export default Button;

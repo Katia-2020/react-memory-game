31023cd137167cec9e24bacfb2b3b3dbee4bcde3
Katia-2020 <kateflights@gmail.com>
2020-11-17 11:49:35 +0000

    abstracted the function that gives correct color to component

--- client/app/components/score/score.jsx
+++ client/app/components/score/score.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Text from '../text';
 import styles from './score.scss';
+import { getResults } from '../utilities/results.utilities';
 
 const Score = (props) => {
   const { score } = props;
 
-  const getColor = () => {
-    if (score < 15) {
-      return 'light-green';
-    }
+  // const getColor = () => {
+  //   if (score < 15) {
+  //     return 'light-green';
+  //   }
 
-    if (score >= 15 && score < 30) {
-      return 'light-yellow';
-    }
+  //   if (score >= 15 && score < 30) {
+  //     return 'light-yellow';
+  //   }
 
-    if (score >= 30 && score <= 45) {
-      return 'light-pink';
-    }
+  //   if (score >= 30 && score <= 45) {
+  //     return 'light-pink';
+  //   }
 
-    return 'light-purple';
-  }
+  //   return 'light-purple';
+  // }
 
   return (
     <div className={styles['score']}>
@@ -27,7 +28,7 @@ const Score = (props) => {
         <Text text="your score:" cases="uppercase" color="dark-blue" weight="bold" />
       </div>
       <div className={styles['score__body']}>
-        <Text text={score} size="large" color={getColor()} weight="bold" />
+        <Text text={score} size="large" color={getResults(score).color} weight="bold" />
       </div>
     </div>
   );

9209138c374401b4aaa679d94046c8c9516965cf
Katia-2020 <kateflights@gmail.com>
2020-11-17 15:38:32 +0000

    created an array with ranges in a separate file, the array sends the info to the components

--- client/app/components/utilities/results.utilities.js
+++ client/app/components/utilities/results.utilities.js
@@ -1,19 +1,26 @@
+const ranges = [
+  { min: 10, max: 18 },
+  { min: 17, max: 30 },
+  { min: 30, max: 45 },
+  { min: 46, max: 400 },
+];
+
 export const getResults = (score) => {
-  if (score < 18) {
+  if (score <= ranges[0].max) {
     return {
       feedback: 'well done!',
       color: 'light-green',
     };
   }
 
-  if (score >= 18 && score < 30) {
+  if (score > ranges[1].min && score <= ranges[1].max) {
     return {
       feedback: 'good!',
       color: 'light-yellow',
     };
   }
 
-  if (score >= 30 && score <= 45) {
+  if (score > ranges[2].min && score <= ranges[2].max) {
     return {
       feedback: 'not too bad!',
       color: 'light-pink',
@@ -28,23 +35,23 @@ export const getResults = (score) => {
 
 export const resultsArray = [
   {
-    result: 5,
+    result: ranges[0].max,
     text: 'amazing memory!',
-    range: '10-17',
+    range: `${ranges[0].min} - ${ranges[0].max}`,
   },
   {
-    result: 20,
+    result: ranges[1].max,
     text: "no worries, that's still good enough",
-    range: '18-29',
+    range: `${ranges[1].min} - ${ranges[1].max}`,
   },
   {
-    result: 40,
+    result: ranges[2].max,
     text: 'you really need to improve',
-    range: '30-45',
+    range: `${ranges[2].min} - ${ranges[2].max}`,
   },
   {
-    result: 70,
+    result: ranges[3].max,
     text: 'memory?! what memory?',
-    range: '46 +',
+    range: `${ranges[3].min} +`,
   },
 ];

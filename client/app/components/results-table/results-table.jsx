c39ec544d681debe26c47f400c36fe187511e964
Katia-2020 <kateflights@gmail.com>
2020-11-17 11:48:01 +0000

    created results-table component

---
+++ client/app/components/results-table/results-table.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './results-table';
+import { getResults } from '../utilities/results.utilities';
+
+const cx = classnames.bind(styles);
+
+const ResultsTable = (props) => {
+  const { } = props;
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ResultsTable;

569e753f8264f3fa9a00c612e78e85e95cdc5fed
Katia-2020 <kateflights@gmail.com>
2020-11-18 00:20:02 +0000

    amended levels array

--- client/app/components/utilities/levels.utilities.js
+++ client/app/components/utilities/levels.utilities.js
@@ -1,5 +1,14 @@
 export const levels = [
-  'easy',
-  'medium',
-  'hard',
+  {
+    id: 0,
+    name: 'easy',
+  },
+  {
+    id: 1,
+    name: 'medium',
+  },
+  {
+    id: 2,
+    name: 'hard',
+  },
 ];

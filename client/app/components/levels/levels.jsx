05643d6a7992eb1a0d48f0bffc43a39abf4442c3
Katia-2020 <kateflights@gmail.com>
2020-11-18 00:55:41 +0000

    updated levels component with changes

--- client/app/components/levels/levels.jsx
+++ client/app/components/levels/levels.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import classnames from 'classnames/bind';
 import styles from './levels.scss';
-import Text from '../text';
-
-const cx = classnames.bind(styles);
+import LevelButton from '../level-button';
 
 const Levels = (props) => {
   const { levels, onClick } = props;
 
-  const handleClick = () => {
-    onClick();
-  };
-
   return (
     <div className={styles.levels}>
       {levels.map(level => (
-        <div key={level.id} className={styles['levels__item']}>
-          <div
-            className={cx('levels__button', {
-              [`levels__button--${level.name}`]: level.name,
-            })}
-            onClick={handleClick}
-          >
-            <Text text={level.name} center size="medium" cases="uppercase" weight="bold" color="dark-blue" />
-          </div>
-        </div>
+        <LevelButton
+          key={level.id}
+          onClick={onClick}
+          level={level}
+        />
       ))}
     </div>
   );
--- client/app/components/utilities/levels.utilities.js
+++ client/app/components/utilities/levels.utilities.js
@@ -1,14 +1,1 @@
-export const levels = [
-  {
-    id: 0,
-    name: 'easy',
-  },
-  {
-    id: 1,
-    name: 'medium',
-  },
-  {
-    id: 2,
-    name: 'hard',
-  },
-];
+export const levels = ['easy', 'medium', 'hard'];

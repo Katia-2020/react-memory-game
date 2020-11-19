f463a21fc130c0e40c02d45598fa55d03b191919
Katia-2020 <kateflights@gmail.com>
2020-11-19 00:49:30 +0000

    added events to levelButton component and converted it into class

--- client/app/components/level-button/level-button.jsx
+++ client/app/components/level-button/level-button.jsx
@@ -5,23 +5,55 @@ import styles from './level-button.scss';
 
 const cx = classnames.bind(styles);
 
-const LevelButton = (props) => {
-  const { level, onClick } = props;
+class LevelButton extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const handleClick = () => {
+    this.state = {
+      isHovered: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+
+  handleClick() {
+    const { level, onClick } = this.props;
     onClick(level);
-  };
+  }
 
-  return (
-    <div
-      className={cx('level-button', {
-        [`level-button--${level}`]: level,
-      })}
-      onClick={handleClick}
-    >
-      <Text text={level} center size="medium" cases="uppercase" weight="bold" color="dark-blue" />
-    </div>
-  );
-};
+  handleMouseEnter() {
+    this.setState({
+      isHovered: true,
+    });
+  }
+
+  handleMouseLeave() {
+    this.setState({
+      isHovered: false,
+    });
+  }
+
+  render() {
+    const { level } = this.props;
+    const { isHovered } = this.state;
+
+    return (
+      <div
+        className={cx('level-button', {
+          [`level-button--${level}`]: level,
+          'level-button--active': isHovered,
+
+        })}
+        onClick={this.handleClick}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <Text text={level} center size="medium" cases="uppercase" weight="bold" color="dark-blue" />
+      </div>
+    );
+  };
+}
 
 export default LevelButton;

170d74958fd96bf5284eee4487c66e81a85349fd
Katia-2020 <kateflights@gmail.com>
2020-12-02 10:51:25 +0000

    fixed eslint issues

--- client/app/components/utilities/results-utilities.test.jsx
+++ client/app/components/utilities/results-utilities.test.jsx
@@ -3,14 +3,14 @@ import { getResults } from './results.utilities';
 describe('results-utilities', () => {
   describe('getResults when called', () => {
     it('should return correct obj', () => {
-       const args = 10;
-       const expected = getResults(args);
-       const received = {
+      const args = 10;
+      const expected = getResults(args);
+      const received = {
         feedback: 'well done!',
         color: 'light-green',
-       };
+      };
 
-       expect(received).toMatchObject(expected);
+      expect(received).toMatchObject(expected);
     });
   });
 });
